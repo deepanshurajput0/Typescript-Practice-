6f65fa2fa8a6c2e9833cb6124ce6aef5e81c0972
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-31 22:55:13 -0800

    Generics problem statement

--- Generics.js
+++ Generics.js
@@ -1,48 +1,6 @@
 /// Generics 
-// Generics in TypeScript are a powerful feature that allows you to create reusable, flexible, and type-safe components, functions, and classes
-function Identity(arg) {
-    return arg;
+function firstElement(arr) {
+    return arr[0];
 }
-var results = Identity('deepanshu');
-console.log(results);
-function add(a, b) {
-    console.log(typeof a);
-    console.log(typeof b);
-}
-var ourType = add(2, 'deepanshu');
-function names(nam, lNam) {
-    console.log(nam, lNam);
-}
-names('deepanshu', 'verma');
-var product1 = {
-    name: 'Laptop',
-    price: 40000,
-    quantity: 5
-};
-console.log(product1);
-function totalPrice(product1) {
-    console.log(product1.quantity * product1.price);
-}
-totalPrice(product1);
-var getValue = function (data) {
-    console.log(data);
-};
-var person1 = {
-    name: 'deepanshu',
-    age: 19
-};
-getValue(person1);
-var user2 = {
-    name: 'deepanshu',
-    age: 19,
-    email: 'deepanshu@gmail.com'
-};
-var user = {
-    name: 'shivam',
-    age: 17
-};
-var func = function (n, o) {
-    return { n: n, o: o };
-};
-var ans = func(user, user2);
-console.log(ans);
+var result2 = firstElement(['deep', 'verma']);
+console.log(result2);
--- Generics.ts
+++ Generics.ts
@@ -2,101 +2,123 @@
 
 // Generics in TypeScript are a powerful feature that allows you to create reusable, flexible, and type-safe components, functions, and classes
 
-function Identity<T> (arg:T): T{
-   return arg
+// function Identity<T> (arg:T): T{
+//    return arg
+// }
+
+// const results = Identity('deepanshu'
+// )
+// console.log(results)
+
+
+// function add <T,U>(a:T,b:U){
+//     console.log( typeof a) 
+//     console.log( typeof b) 
+// }
+
+
+// const ourType = add<number ,string >(2,'deepanshu')
+
+
+
+// function names<T,L> (nam:T, lNam:L){
+//     console.log(nam , lNam) 
+// }
+
+// names('deepanshu','verma')
+
+
+// interface Product {
+//     name:string,
+//     price:number,
+//     quantity:number
+// }
+
+
+// const product1:Product = {
+//     name:'Laptop',
+//     price:40000,
+//     quantity:5
+// }
+
+// console.log(product1)
+
+// function totalPrice (product1:Product){
+//     console.log(product1.quantity * product1.price)
+// }
+
+
+// totalPrice(product1)
+
+
+
+
+
+
+// // Generics in Details 
+
+// type Info={
+//     name:string,
+//     age:number
+// }
+
+// const getValue =<T>(data:T,)=>{
+//        console.log(data)
+// }
+
+// const person1:Info ={
+//   name:'deepanshu',
+//   age:19
+// }
+
+// getValue<Info>(person1)
+
+// type Person2 ={
+//     name:string,
+//     age:number,
+//     email:string
+// }
+// const user2:Person2 ={
+//    name:'deepanshu',
+//    age:19,
+//    email:'deepanshu@gmail.com'
+// }
+
+// type Person1 ={
+//     name:string,
+//     age:number
+// }
+// const user:Person1 = {
+//    name:'shivam',
+//    age:17
+// }
+
+// const func=<T, U extends T>(n:T, o:U)=>{
+//     return { n, o }
+// }
+
+// const ans = func<Person1,Person2>(user,user2)
+
+// console.log(ans)
+
+
+/// Why we have to use generics 
+
+
+// Now In this code i have to make my string in uppercase but i am unable to do it beacause of Type of Arr it has two types one is string other is number
+
+// Thats why we have to use generics to handle these situations 
+
+// type Arr = string | number
+
+// This is example how we can use Generics 
+
+function firstElement<T>(arr:T[]){
+  return arr[0]
 }
 
-const results = Identity('deepanshu'
-)
-console.log(results)
+const result2 = firstElement<string>(['deep','verma'])
+const result3 = firstElement<number>([1,2])
+console.log(result2)
 
 
-function add <T,U>(a:T,b:U){
-    console.log( typeof a) 
-    console.log( typeof b) 
-}
-
-
-const ourType = add<number ,string >(2,'deepanshu')
-
-
-
-function names<T,L> (nam:T, lNam:L){
-    console.log(nam , lNam) 
-}
-
-names('deepanshu','verma')
-
-
-interface Product {
-    name:string,
-    price:number,
-    quantity:number
-}
-
-
-const product1:Product = {
-    name:'Laptop',
-    price:40000,
-    quantity:5
-}
-
-console.log(product1)
-
-function totalPrice (product1:Product){
-    console.log(product1.quantity * product1.price)
-}
-
-
-totalPrice(product1)
-
-
-
-
-
-
-// Generics in Details 
-
-type Info={
-    name:string,
-    age:number
-}
-
-const getValue =<T>(data:T,)=>{
-       console.log(data)
-}
-
-const person1:Info ={
-  name:'deepanshu',
-  age:19
-}
-
-getValue<Info>(person1)
-
-type Person2 ={
-    name:string,
-    age:number,
-    email:string
-}
-const user2:Person2 ={
-   name:'deepanshu',
-   age:19,
-   email:'deepanshu@gmail.com'
-}
-
-type Person1 ={
-    name:string,
-    age:number
-}
-const user:Person1 = {
-   name:'shivam',
-   age:17
-}
-
-const func=<T, U extends T>(n:T, o:U)=>{
-    return { n, o }
-}
-
-const ans = func<Person1,Person2>(user,user2)
-
-console.log(ans)
