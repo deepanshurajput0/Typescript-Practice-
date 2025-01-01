e9f1936a178504dc444c644c1858b4252039d228
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-01 00:12:21 -0800

    Implement Record type for dynamic object keys with user data

---
+++ record.js
@@ -0,0 +1,11 @@
+var userz = {
+    'user1': {
+        name: 'deep',
+        email: "deep@gmail.com"
+    },
+    'user2': {
+        name: 'viv',
+        email: 'viv@gmail.com'
+    }
+};
+console.log(userz);
--- record.ts
+++ record.ts
@@ -1,5 +1,3 @@
-// Record 
-
 type User2 ={
     name:string,
     email:string
@@ -20,3 +18,19 @@ const users:userObject = {
         email:'viv@gmail.com'
     }
 }
+
+// / By using Records 
+
+type Users = Record<string,User2>
+
+const userz = {
+    'user1':{
+        name:'deep',
+        email:"deep@gmail.com"
+     },
+     'user2':{
+         name:'viv',
+         email:'viv@gmail.com'
+     }
+}
+console.log(userz)
