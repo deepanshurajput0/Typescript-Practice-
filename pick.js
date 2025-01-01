643a9125c0df53d9055c3a0f9d33479af511c19b
deepanshu verma <deepanshuverma186@gmail.com>
2024-12-31 23:20:37 -0800

    usecase of pick in typescript

---
+++ pick.js
@@ -0,0 +1,11 @@
+// interface User {
+//     name:string,
+//     age:number
+// }
+function updatedUser(user) {
+    console.log("".concat(user.name, " + ").concat(user.email));
+}
+updatedUser({
+    name: 'deep',
+    email: 'deep@gmail.com'
+});
---
+++ pick.ts
@@ -0,0 +1,45 @@
+// interface User {
+//     name:string,
+//     age:number
+// }
+
+// function sumOfAge(user1:User,user2:User){
+//    return user1.age + user2.age
+// }
+
+// const result4 = sumOfAge({
+//     name:'deep',
+//     age:20
+// },{
+//     name:'raman',
+//     age:20
+// })
+
+// console.log(result4)
+
+
+
+/// Using Pick 
+
+// Pick allows you to create a new type by selecting a set of properties (Keys) from an existing type (Type).
+// Imagine you have a User model with several properties, but for a user profile display, you only need a subset of these properties.
+
+
+interface User {
+    name:string,
+    email:string,
+    password:string,
+    age:number,
+    Single:boolean
+}
+
+type UpdatedUser = Pick<User , 'name' | 'email' > 
+
+function updatedUser(user:UpdatedUser){
+   console.log(`${user.name} + ${user.email}`)
+}
+
+updatedUser({
+    name:'deep',
+    email:'deep@gmail.com'
+})
