341bbae039316f9bc2eed1e9306cd4d1e8fd8cbb
deepanshu verma <deepanshuverma186@gmail.com>
2025-01-01 20:57:37 -0800

    Revision of Readonly & Partial

--- revision.ts
+++ revision.ts
@@ -14,6 +14,33 @@ interface userData {
 
 type updatedData = Pick<userData, 'email'|'password'>
 
-function updatedUserData(){
+/// Using Partial 
+
+type newUserData = Partial<updatedData>
+
+function updatedUserData(user:updatedData){
 
 }
+
+
+interface Book {
+    name:string,
+    author:string,
+    price:number
+}
+
+const bookData:Readonly<Book> = {
+    name:'Geeky world',
+    author:'Dustin Markz',
+    price:200
+}
+
+
+// here we can able to add new data. but the question is how to restrict it 
+// for this we are using readOnly
+
+// bookData.author = 'Linus Torvalds'
+
+console.log(bookData)
+
+
